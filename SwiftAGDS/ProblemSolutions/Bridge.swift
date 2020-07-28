3bb61de9a6a6aaae521257a5b128e01f8f92c5d2
Cayo M Amorim <cayomesquita@gmail.com>
2020-07-28 07:43:09 -0700

    Complete Bridge

---
+++ SwiftAGDS/ProblemSolutions/Bridge.swift
@@ -0,0 +1,131 @@
+//
+//  Bridge.swift
+//  SwiftAGDS
+//
+//  Created by Cornerstone on 2020-07-26.
+//  Copyright © 2020 Cornerstone. All rights reserved.
+//
+
+import Foundation
+
+class Bridge {
+ 
+    var grid = [[Int]]() // -1 not visited, 0 water, 1..n island
+    fileprivate let queue = Queue<Position>.init()
+    let dy = [0, 0, 1, -1]
+    let dx = [1, -1, 0, 0]
+    
+    fileprivate func bfsFillIslands(_ gridIn: [[Int]]) -> [[Int]] {
+        let size = gridIn.count
+        var grid = [[Int]](repeating: [Int](repeating: -1, count: size), count: size)
+        var tagIsland = 1
+        for y in 0..<size {
+            for x in 0..<size {
+                if grid[y][x] < 0 { // not visited
+                    grid[y][x] = 0
+                    if gridIn[y][x] > 0 { // island
+                        self.queue.enqueue(item: Position.init(y: y, x: x))
+                        while let cell = self.queue.dequeue() {
+                            let x = cell.x
+                            let y = cell.y
+                            grid[y][x] = tagIsland
+                            for i in 0..<dy.count {
+                                let ny = y + dy[i]
+                                let nx = x + dx[i]
+                                if ny >= 0, nx >= 0, ny < size, nx < size, grid[ny][nx] < 0 { // not visited
+                                    if gridIn[ny][nx] > 0 {
+                                        self.queue.enqueue(item: Position.init(y: ny, x: nx))
+                                    } else {
+                                        grid[ny][nx] = 0
+                                    }
+                                }
+                            }
+                        }
+                        tagIsland += 1
+                    }
+                }
+            }
+        }
+        return grid
+    }
+    
+    fileprivate func bfsLowestDistance(_ y: Int, _ x: Int, _ size: Int, _ distance: inout Int) {
+        var grid = self.grid
+        let tag = grid[y][x]
+        self.queue.enqueue(item: Position.init(y: y, x: x))
+        while let cell = self.queue.dequeue() {
+            let x = cell.x
+            let y = cell.y
+            let nDistance = grid[y][x] < 0 ? grid[y][x] - 1 : -1
+            if distance != Int.max, abs(nDistance) > distance  {
+                continue
+            }
+            for i in 0..<dy.count {
+                let ny = y + dy[i]
+                let nx = x + dx[i]
+                if ny >= 0, nx >= 0, ny < size, nx < size{
+                    if grid[ny][nx] == 0 {
+                        self.queue.enqueue(item: Position.init(y: ny, x: nx))
+                        grid[ny][nx] = nDistance
+                    } else if grid[ny][nx] > 0, grid[ny][nx] != tag {
+                        distance = min(distance, abs(grid[y][x]))
+                    }
+                }
+            }
+        }
+//        print("")
+//        print(Position.init(y: y, x: x))
+//        for row in grid {
+//            print(row)
+//        }
+    }
+    
+    func solution(grid gridIn: [[Int]]) -> Int {
+        self.grid = bfsFillIslands(gridIn)
+        for row in gridIn {
+            print(row)
+        }
+        print("")
+        for row in self.grid {
+            print(row)
+        }
+        let size = self.grid.count
+        var distance = Int.max
+        for y in 0..<size {
+            for x in 0..<size {
+                if self.grid[y][x] > 0 {
+                    bfsLowestDistance(y, x, size, &distance)
+                }
+            }
+        }
+        return distance
+    }
+    
+    func solutionByInputFile(inFile: String, outFile: String? = nil) -> Int {
+        guard let lines = readFile(inFile) else {
+            return -1
+        }
+        let size = Int(lines[0])!
+        self.grid = [[Int]](repeating: [Int](repeating: 0, count: size), count: size)
+        
+        for h in 1...size {
+            self.grid[h - 1] = lines[h].components(separatedBy: .whitespaces).map {Int($0)!}
+        }
+        
+        let result = solution(grid: self.grid)
+        
+        if let outFile = outFile, let line = readFile(outFile)?.first, let answer = Int(line) {
+            if result == answer {
+                print("Success")
+            } else {
+                print("Fail (expected:\(answer) - result:\(result))")
+            }
+        }
+        return result
+    }
+}
+
+fileprivate struct Position {
+    let y, x: Int
+}
+
--- SwiftAGDS/main.swift
+++ SwiftAGDS/main.swift
@@ -7,13 +7,39 @@
 //
 
 import Foundation
-for i in 1...5 {
-    let testCase = "/Users/cornerstone/Workspace/WMAD/SwiftAGDS/SwiftAGDS/ProblemSolutions/Inputs/TomatoFarm/tomatoFarm.\(i)"
+for i in 1...2 {
+    let testCase = "/Users/cornerstone/Workspace/WMAD/SwiftAGDS/SwiftAGDS/ProblemSolutions/Inputs/BridgeTestCases/bridge.\(i)"
     if let url = URL.init(string: testCase) {
         print("\([Character](repeating: "-", count: 40).reduce("") {$0 + String.init($1)})")
         print("TestCase: \(url.lastPathComponent)")
     }
-    print(TomatoFarm().solutionByInputFile(inFile: "\(testCase).in", outFile: "\(testCase).out"))
+    print(Bridge().solutionByInputFile(inFile: "\(testCase).in", outFile: "\(testCase).out"))
 }
+
+//var uf = UF.init(10)
+//print(uf)
+//uf.union(2, 8)
+//print(uf)
+//uf.union(2, 3)
+//print(uf)
+//uf.union(1, 0)
+//print(uf)
+//uf.union(1, 4)
+//print(uf)
+//uf.union(4, 5)
+//print(uf)
+//uf.union(4, 6)
+//print(uf)
+//uf.union(4, 9)
+//print(uf)
+//uf.union(2, 7)
+//print(uf)
+//uf.union(7, 6)
+//print(uf)
+//print(uf.find(3))
+//print(uf.find(8))
+//print(uf.find(0))
+//print(uf.find(9))
+
 
 
