354aee769c2e4e1890a4719ce5d9cb22ee27f715
Cayo M Amorim <cayomesquita@gmail.com>
2020-08-03 07:49:13 -0700

    Complete MST - Kruskal and fix Prim

--- SwiftAGDS/ProblemSolutions/MST.swift
+++ SwiftAGDS/ProblemSolutions/MST.swift
@@ -19,36 +19,69 @@ public final class MST {
         var graph = graph
         var visited = [Bool](repeating: false, count: graph.count)
         var sum = 0
-        for u in 0..<visited.count {
-            if !visited[u] {
-                dfsPrimMST(u, &graph, &visited, &sum)
+        
+        var u = 0
+        visited[u] = true
+        var edges = BH<Tuple>(graph[u].map {Tuple.init(u: u, v: $0.v, w: $0.w)})
+        while let edge = edges.pop() {
+            let v = edge.v
+            let weight = edge.w
+            if !visited[v] {
+                visited[v] = true
+                sum += weight
+                for tuple in graph[v].map {Tuple.init(u: v, v: $0.v, w: $0.w)} {
+                    if !visited[tuple.v] {
+                        edges.push(tuple)
+                    }
+                }
             }
         }
         return sum
     }
     
-    fileprivate func dfsPrimMST(_ u: Int, _ graph: inout [[(v: Int, w: Int)]], _ visited: inout [Bool], _ sum: inout Int) {
-        visited[u] = true
-        let edges = BH(graph[u].map {Tuple.init(v: $0.v, w: $0.w)})
-        while let edge = edges.pop() {
-            if !visited[edge.v] {
-                sum += edge.w
-                dfsPrimMST(edge.v, &graph, &visited, &sum)
-            }
-        }
-    }
+//    fileprivate func dfsPrimMST(_ u: Int, _ graph: inout [[(v: Int, w: Int)]], _ visited: inout [Bool], _ edges: BH<Tuple>, _ sum: inout Int) {
+//        visited[u] = true
+//        for tuple in graph[u].map {Tuple.init(u: u, v: $0.v, w: $0.w)} {
+//            edges.push(tuple)
+//        }
+//        while let edge = edges.pop() {
+//            if !visited[edge.v] {
+//                sum += edge.w
+//                dfsPrimMST(edge.v, &graph, &visited, &sum)
+//            }
+//        }
+//    }
 
     /// Kruskal's MST Algorithm O(ElgE)
     /// Use Union-Find and Adjacency List (You can use Swift's built-in sort method.)
     /// - Parameter graph: adjacency list of weighted undirected graph where each edge is stored as Tuple
     /// - Returns: the minimum cost spanning tree
     public func kruskalMST(_ graph: [[(v: Int, w: Int)]]) -> Int {
-        return -1
+        var edges = [Tuple]()
+        for u in 0..<graph.count {
+            for edge in graph[u] {
+                edges.append(Tuple(u: u, v: edge.v, w: edge.w))
+            }
+        }
+        edges.sort()
+        var uf = UF(graph.count)
+        var sum = 0
+        for edge in edges {
+            let u = edge.u
+            let v = edge.v
+            let weight = edge.w
+            if !uf.connected(u, v) {
+                uf.union(u, v)
+                sum += weight
+            }
+        }
+        return sum
     }
     
 }
 
-struct Tuple {
+fileprivate struct Tuple {
+    let u: Int
     let v: Int
     let w: Int
 }
--- SwiftAGDS/main.swift
+++ SwiftAGDS/main.swift
@@ -19,8 +19,23 @@ import Foundation
 let graph: [[(v: Int, w: Int)]] = [[(1,2), (3,1)]
     , [(0,2), (3,2)]
     , [(3,3)]
-    , [(0,1), (1,2), (2,3)]]
+    , [(0,1), (1,2), (2,3)]
+]
 
-print(MST().primMST(graph))
+let graph2: [[(v: Int, w: Int)]] = [[(1,7), (3,5)]
+    , [(0,7), (2,8), (3,9), (4,7)]
+    , [(1,8), (4,5)]
+    , [(0,5), (1,9), (4,15), (5,6)]
+    , [(1,7), (2,5), (3,15), (5,8), (6,9)]
+    , [(3,6), (4,8), (6,11)]
+    , [(4,9), (5,11)]
+]
+
+let mst = MST()
+print(mst.primMST(graph))
+print(mst.kruskalMST(graph))
+print(mst.primMST(graph2))
+print(mst.kruskalMST(graph2))
+
 
 
